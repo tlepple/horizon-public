17bccc3ae4c2ed6497c96e653da174c4765047a5
root <rtlepple@gmail.com>
2019-12-21 17:07:53 +0000

    initial commit

---
+++ provider/aws/variables.tf
@@ -0,0 +1,48 @@
+variable "owner_name" {
+  description = "Owner name of resources"
+}
+
+variable "owner_initials" {
+  description = "Owner initials"
+}
+
+variable "aws_access_key_id" {
+  description = "Abort this with CTRL-C, set the TF_VAR_aws_access_key_id environment variable in your shell and try again."
+}
+
+variable "aws_secret_access_key" {
+  description = "Abort this with CTRL-C, set the TF_VAR_aws_secret_access_key environment variable in your shell and try again."
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+}
+
+variable "aws_vpc_name" {
+  description = "AWS VPC Name"
+}
+
+variable "aws_vpc_id" {
+  description = "AWS VPC ID"
+}
+
+variable "aws_subnet_name" {
+  description = "AWS  Subnet Name"
+}
+
+variable "aws_cidrs" {
+  description = "AWS CIDR IP Range"
+  type = list
+}
+
+variable "aws_s3_bucketname" {
+  description = "AWS S3 Bucketname"
+}
+
+variable "tag_project" {
+  description = "Project Name"
+}
+
+variable "tag_enddate" {
+  description = ""Resource expiration date (MMDDYYYY)"
+}
